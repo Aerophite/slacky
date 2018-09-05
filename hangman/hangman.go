695a65ab4639cfc69b96cb452683c943a2995816
Michael Morelock <mmorelock@goodlooking.company>
2018-09-05 15:07:22 -0500

    test

--- hangman/hangman.go
+++ hangman/hangman.go
@@ -248,7 +248,7 @@ func start(message globals.Message) error { // sentence, number of guesses
             sentence,
             generateHangmanString(strings.ToLower(sentence), "[abcdefghijklmnopqrstuvwxyz]"),
             guessesRemaining,
-            map[string]bool {"a": false, "b": false, "c": false, "d": false, "e": false, "f": false, "g": false, "h": false, "i": false, "j": false, "k": false, "m": false, "n": false, "o": false, "p": false, "q": false, "r": false, "s": false, "t": false, "u": false, "v": false, "w": false, "x": false, "y": false, "z": false},
+            map[string]bool {"a": false, "b": false, "c": false, "d": false, "e": false, "f": false, "g": false, "h": false, "i": false, "j": false, "k": false, "l": false, "m": false, "n": false, "o": false, "p": false, "q": false, "r": false, "s": false, "t": false, "u": false, "v": false, "w": false, "x": false, "y": false, "z": false},
             message.Channel,
             message.User,
             []globals.User{},
